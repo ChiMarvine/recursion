890da5b70977c3922d0039734093bfa139a7cc6a
Marvine Chi <chi.marvine@gmail.com>
2015-06-10 20:22:46 -0700

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+   var input = typeof obj;
+
+   //Possible cases
+   if(obj === null) {
+      return "null";
+   }
+   if(input === 'boolean' || input === 'number') {
+      return obj.toString();
+   }
+   if(input === 'string') {
+      return '"' + obj + '"';
+   }
+
+   //stringify arrays
+   if(Array.isArray(obj)) {
+      return '[' + obj.map(stringifyJSON) + ']';
+   }
+
+   //stringify objects
+   if(input === 'object') {
+      var output = "";
+      for(var key in obj) {
+         if(typeof obj[key] !== 'function' && obj[key] !== undefined) {
+            output += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ",";
+         }
+      }
+      if(output.slice(-1) === ',') {
+         output = output.slice(0, -1);
+      }
+
+      return '{' + output + '}';
+   }
 };
